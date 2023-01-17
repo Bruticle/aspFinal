8da926daf4c107132d3d2f8f563a2a6e9a20cf1b
Bruticle <erenerdem1000@gmail.com>
2023-01-17 23:01:54 +0300

    Proje dosyası ekle.

---
+++ application/Models/Ders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace application.Models
+{
+    public class Ders
+    {
+        [Key]
+        public int DersId { get; set; }
+        public string DersAdi { get; set; }
+        public int? Kredisi { get; set; }
+        public int OkulYonetimId { get; set; }
+        public virtual OkulYonetim OkulYonetim { get; set; }
+    }
+}
